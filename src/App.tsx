d59e5257ff306fbc7160b9dedc22b1ae67858755
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-04-24 11:04:56 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        <Spline scene="https://draft.spline.design/XwFWIp6Iey4zZuru/scene.spline" />
+      </div>
+    </div>
+  );
+}
